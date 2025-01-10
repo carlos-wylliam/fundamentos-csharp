2744c3da0e19c04be6f48e6c3b7f58b306adb4fb
Wylliam <carloswylliam023@gmail.com>
2025-01-10 15:32:13 -0300

    exercise with ternary operator

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ namespace AgeRange
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Informe a sua idade: ");
+            
+            int age = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+            string result = age < 18 ? "Você é menor de idade": age <= 60 ? "Você é adulto" : "Você é idoso";
+            Console.WriteLine(result);
+        }
+    }
+ }

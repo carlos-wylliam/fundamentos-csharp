2202669e678dda373793c4c32f9ecf02a67d0d32
Wylliam <carloswylliam023@gmail.com>
2025-01-10 17:05:04 -0300

    odd or even exercise

---
+++ ImparOuPa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ImparOuPa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Olá, informe um número inteiro:");
+            
+            int number = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+
+            string result = number % 2 == 0 ? "O número é par" : "O número é ímpar";
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+
+

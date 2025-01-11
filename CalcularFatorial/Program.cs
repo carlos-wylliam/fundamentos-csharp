8beb4531ad6a1bcc29cf641fdb7f2d6e47a1943b
Wylliam <carloswylliam023@gmail.com>
2025-01-11 14:00:09 -0300

    exercise factorial calculate

---
+++ CalcularFatorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FactorialCalculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um número inteiro positivo");
+
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"O fatorial do número {number} é {factorial}");
+
+        }
+    }
+}
+

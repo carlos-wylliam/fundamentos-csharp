3787fc688d6e51b565b4e4970ee49fd44f1a50a1
Wylliam <carloswylliam023@gmail.com>
2025-01-15 12:44:28 -0300

    sequência de fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um número para a sequência");
+            int numero = int.Parse(Console.ReadLine());
+
+            int primeiroNumero = 0;
+            int segundoNumero = 1;
+
+            Console.WriteLine("Sequência de Fibonacci:");
+
+            Console.WriteLine(primeiroNumero);
+
+            for (int i = 1; i <= numero ; i++)
+            {
+                Console.WriteLine(segundoNumero);
+
+                int proximoNumero = primeiroNumero + segundoNumero;
+
+                primeiroNumero = segundoNumero;
+                segundoNumero = proximoNumero;
+            }
+        }
+    }
+}
+
+
